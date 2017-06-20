9dbc8680da75013f3ddd5b5eea9dcd2e2112379c
姜灏 <150haomengting@163.com>
2017-06-20 11:31:04 +0800

    add mini program

---
+++ mp/mp.go
@@ -0,0 +1,103 @@
+package mp
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+var (
+	// AppID appid
+	AppID string
+
+	// AppSecret app secret
+	AppSecret string
+
+	// SessionKey sessionKey
+	SessionKey string
+)
+
+// WMP watermark mp
+type WMP struct {
+	MP
+	Watermark Watermark `json:"watermark"` // 水印
+}
+
+// MP  mini program
+type MP struct {
+	NickName  string `json:"nickName"`          // 用户昵称
+	Gender    int    `json:"gender"`            // 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+	Language  string `json:"language"`          // 语言
+	City      string `json:"city"`              // 普通用户个人资料填写的城市
+	Province  string `json:"province"`          // 用户个人资料填写的省份
+	Country   string `json:"country"`           // 国家，如中国为CN
+	AvatarURL string `json:"avatarUrl"`         // 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+	OpenID    string `json:"openid,omitempty"`  // 用户的唯一标识
+	UnionID   string `json:"unionid,omitempty"` // 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。详见：获取用户个人信息（UnionID机制）
+}
+
+// Watermark water mark
+type Watermark struct {
+	AppID     string `json:"appid,omitempty"`
+	TimeStamp int    `json:"timestamp,omitempty"`
+}
+
+// NewMP new mini program
+func NewMP() *MP {
+	return &MP{}
+}
+
+// Verify verify
+// date 2017-06-19
+// author andy.jiang
+func (m MP) Verify(rawData MP, signature string) bool {
+	body, err := json.Marshal(rawData)
+	if err != nil {
+		return false
+	}
+	fmt.Println(fmt.Sprintf("%s%s",
+		string(body),
+		SessionKey))
+	data := rsae.NewRsae().SHA1(fmt.Sprintf("%s%s",
+		string(body),
+		SessionKey,
+	))
+	if signature != fmt.Sprintf("%x", data) {
+		return false
+	}
+	return true
+}
+
+// User user
+// date 2017-06-19
+// author andy.jiang
+func (m MP) User(encryptedData, iv string) (MP, error) {
+	result := MP{}
+	encryptedByte, err := rsae.NewRsae().Base64Decode(encryptedData)
+	if err != nil {
+		return result, err
+	}
+	sessionByte, err := rsae.NewRsae().Base64Decode(SessionKey)
+	if err != nil {
+		return result, err
+	}
+	ivByte, err := rsae.NewRsae().Base64Decode(iv)
+	if err != nil {
+		return result, err
+	}
+	body, err := rsae.NewRsae().AESDecrypt(encryptedByte, sessionByte, ivByte)
+	if err != nil {
+		return result, err
+	}
+	data := WMP{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return result, err
+	}
+	if data.Watermark.AppID != AppID {
+		return result, errors.New("appid not match")
+	}
+	return data.MP, nil
+}
---
+++ mp/session.go
@@ -0,0 +1,57 @@
+package mp
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/tiantour/buffer"
+	"github.com/tiantour/fetch"
+)
+
+// Session session
+type Session struct {
+	OpenID     string `json:"openid"`      // 开放ID
+	SessionKey string `json:"session_key"` // session
+	ErrCode    int    `json:"errcode"`     // 错误码
+	ErrMsg     string `json:"errmsg"`      // 错误提示
+}
+
+// NewSession new session
+func NewSession() *Session {
+	return &Session{}
+}
+
+// Get get
+// date 2017-06-19
+// author andy.jiang
+func (s Session) Get(code string) (Session, error) {
+	result := Session{}
+	url := fmt.Sprintf("https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
+		AppID,
+		AppSecret,
+		code,
+	)
+	body, err := fetch.Cmd(fetch.Request{
+		URL:    url,
+		Method: "GET",
+	})
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return result, err
+	}
+	if result.ErrCode != 0 {
+		return result, errors.New(result.ErrMsg)
+	}
+	// 写入缓存
+	_, _ = buffer.NewHash().Add(buffer.Buffer{
+		Action: fmt.Sprintf("mp:%s", AppID),
+		Map: map[string]interface{}{
+			result.OpenID: result.SessionKey,
+		},
+	}).Str()
+	return result, err
+}
